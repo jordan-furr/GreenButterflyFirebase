a857d48725d71da3ebdedeb01177e046e52a7701
Jordan Furr <you@example.com>
2020-05-21 15:50:07 -0400

    d

---
+++ GreenButterflyFirebase/Habit.swift
@@ -0,0 +1,13 @@
+//
+//  Habit.swift
+//  GreenButterflyFirebase
+//
+//  Created by Jordan Furr on 5/21/20.
+//  Copyright © 2020 Jordan Furr. All rights reserved.
+//
+
+import Foundation
+
+class Habit {
+    
+}
---
+++ GreenButterflyFirebase/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  GreenButterflyFirebase
+//
+//  Created by Jordan Furr on 5/21/20.
+//  Copyright © 2020 Jordan Furr. All rights reserved.
+//
+
+import Foundation
